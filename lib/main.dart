9f2b990c7dda56dbb8b2769884df9cc63f4d79e9
Bobonovagulnoza <bobonovagulnoza68@gmail.com>
2024-12-25 20:21:13 +0500

    10 chi vazifaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,251 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
-void main() => runApp(const MyApp());
+final GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => CategoriesPage(),
+    ),
+    GoRoute(
+      path: '/page2',
+      builder: (context, state) => HomePage(),
+    ),
+  ],
+);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const RecipeApp());
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router, // Integrate the GoRouter with MaterialApp
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
     );
   }
 }
+
+
+class CategoriesPage extends StatelessWidget {
+  const CategoriesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1C0F0D),
+      extendBody: true,
+      appBar: AppBar(
+        leadingWidth: 50,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Center(
+            child: SizedBox(
+              width: 21,
+              height: 14,
+              child: SvgPicture.asset(
+                "assets/vector.svg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        title: const Text(
+          "Categories",
+          style: TextStyle(
+            color: Color(0xFFFD5D69),
+          ),
+        ),
+        actions: const [
+          AppBarActionItem(
+            image: "assets/notification.svg",
+            width: 12,
+            height: 17,
+          ),
+          SizedBox(width: 5),
+          AppBarActionItem(
+            image: "assets/search.svg",
+            width: 14,
+            height: 18,
+          ),
+          SizedBox(
+            width: 20,
+          )
+        ],
+        centerTitle: true,
+        backgroundColor: const Color(0xFF1C0F0D),
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          MainCategoryItem(image: "assets/seafood.png", text: "Seafood"),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CategoryItem(image: "assets/lunch.png", text: "Lunch"),
+              CategoryItem(image: "assets/breakfast.png", text: "Breakfast"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CategoryItem(image: "assets/dinner.png", text: "Dinner"),
+              CategoryItem(image: "assets/vegan.png", text: "Vegan"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CategoryItem(image: "assets/dessert.png", text: "Dessert"),
+              CategoryItem(
+                image: "assets/drinks.png",
+                text: "Drinks",
+              ),
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          width: 281,
+          height: 56,
+          decoration: BoxDecoration(
+            color: const Color(0xFFFD5D69),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: SvgPicture.asset("assets/home.svg"),
+                onPressed: () {},
+              ),
+              SvgPicture.asset("assets/community.svg"),
+              SvgPicture.asset("assets/categories.svg"),
+              SvgPicture.asset("assets/profile.svg"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AppBarActionItem extends StatelessWidget {
+  const AppBarActionItem({
+    super.key,
+    required this.image,
+    required this.width,
+    required this.height,
+  });
+
+  final String image;
+  final double width, height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 28,
+      height: 28,
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFC6C9),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Center(
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: SvgPicture.asset(
+            image,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.image, required this.text});
+
+  final String image, text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: SizedBox(
+            width: 159,
+            height: 145,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(13),
+              child: Image(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 6),
+        Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            decoration: TextDecoration.none,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MainCategoryItem extends StatelessWidget {
+  const MainCategoryItem({super.key, required this.image, required this.text});
+
+  final String image, text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            decoration: TextDecoration.none,
+          ),
+        ),
+        const SizedBox(height: 6),
+        Center(
+          child: SizedBox(
+            width: 356,
+            height: 149,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(13),
+              child: Image(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// ---------------------------------------------------------------------------------------------
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -76,8 +308,7 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body:
-      SingleChildScrollView(
+      body: SingleChildScrollView(
           child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -260,7 +491,7 @@ class HomePage extends StatelessWidget {
                               ),
                               child: Center(
                                 child: SvgPicture.asset(
-                                  "assets/heart.svg",
+                                  "assets/search.svg",
                                   width: 15,
                                   height: 15,
                                 ),
@@ -305,13 +536,13 @@ class HomePage extends StatelessWidget {
                                         ),
                                         const SizedBox(width: 3),
                                         SvgPicture.asset(
-                                          "assets/star.svg",
+                                          "assets/search.svg",
                                           width: 10,
                                           height: 10,
                                         ),
                                         const SizedBox(width: 30),
                                         SvgPicture.asset(
-                                          "assets/clock.svg",
+                                          "assets/search.svg",
                                           width: 10,
                                           height: 10,
                                         ),
@@ -357,7 +588,7 @@ class HomePage extends StatelessWidget {
                               ),
                               child: Center(
                                 child: SvgPicture.asset(
-                                  "assets/heart.svg",
+                                  "assets/search.svg",
                                   width: 15,
                                   height: 15,
                                 ),
@@ -402,13 +633,13 @@ class HomePage extends StatelessWidget {
                                         ),
                                         const SizedBox(width: 3),
                                         SvgPicture.asset(
-                                          "assets/star.svg",
+                                          "assets/search.svg",
                                           width: 10,
                                           height: 10,
                                         ),
                                         const SizedBox(width: 30),
                                         SvgPicture.asset(
-                                          "assets/clock.svg",
+                                          "assets/searc.hsvg",
                                           width: 10,
                                           height: 10,
                                         ),
@@ -474,7 +705,7 @@ class HomePage extends StatelessWidget {
                   Column(
                     children: [
                       Image(
-                        image: AssetImage("assets/andrew.png"),
+                        image: AssetImage("assets/joseph.png"),
                         width: 82,
                         height: 74,
                       ),
@@ -491,7 +722,7 @@ class HomePage extends StatelessWidget {
                   Column(
                     children: [
                       Image(
-                        image: AssetImage("assets/emily.png"),
+                        image: AssetImage("assets/joseph.png"),
                         width: 82,
                         height: 74,
                       ),
@@ -508,7 +739,7 @@ class HomePage extends StatelessWidget {
                   Column(
                     children: [
                       Image(
-                        image: AssetImage("assets/jessica.png"),
+                        image: AssetImage("assets/joseph.png"),
                         width: 82,
                         height: 74,
                       ),
@@ -541,19 +772,12 @@ class HomePage extends StatelessWidget {
                       SizedBox(
                         height: 10,
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Image.asset("assets/a2.png"),
-                          Image.asset("assets/a1.png"),
-                        ],
-                      )
-
+                      Image.asset("assets/a2.png"),
+                      Image.asset("assets/a1.png"),
                     ],
                   ),
                 ),
               ),
-
             ],
           )
         ],
@@ -561,4 +785,71 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+// -----------------------------------------------------
 
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+//
+// final GoRouter router = GoRouter(
+//   routes: [
+//     GoRoute(
+//       path: '/',
+//       builder: (context, state) => Page1(),
+//     ),
+//     GoRoute(
+//       path: '/page2',
+//       builder: (context, state) => Page2(),
+//     ),
+//   ],
+// );
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp.router(
+//       routerConfig: router,
+//     );
+//   }
+// }
+//
+// class Page1 extends StatelessWidget {
+//   const Page1({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Text("Salom, Navigation!"),
+//       ),
+//       floatingActionButton: ElevatedButton(
+//         onPressed: () => context.go('/page2'),
+//         child: Text("+"),
+//       ),
+//       // floatingActionButton: ElevatedButton(onPressed: (), child: child),
+//     );
+//   }
+// }
+//
+// class Page2 extends StatelessWidget {
+//   const Page2({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Text("Ikkinchi sahifa!"),
+//       ),
+//       floatingActionButton: ElevatedButton(
+//         onPressed: () => context.go('/'),
+//         child: Text("+"),
+//       ),
+//       // floatingActionButton: ElevatedButton(onPressed: (), child: child),
+//     );
+//   }
+// }
